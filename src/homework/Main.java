4ecde91d578349b5014230d40303f57643048e84
naoko8 <55441804+naoko8@users.noreply.github.com>
2019-12-23 12:55:52 +0400

    Update Main.java

--- src/homework/Main.java
+++ src/homework/Main.java
@@ -1,18 +1,66 @@
 package homework;
-
 import java.util.Scanner;
+import java.util.ArrayList;
 
-public class Main {
 
-    public static void main(String[] args){
+public class HomeWork_5 {
 
-        A number =  new A();
-        number.first();
-        B number1 = new B();
-        number1.first1();
-        number1.first();
-        System.out.println(number1.fifth());
+    String mainString;
+    ArrayList myArrForNumbers = new ArrayList();
+    ArrayList myArr = new ArrayList();
+
+    public void getInput(){
+
+        Scanner input = new Scanner(System.in);
+        mainString=input.next();
+    }
+    public void parseInput() {
+
+        String[] par = mainString.split("-|\\+|\\*|\\/");
+        for (String arr : par) {
+            try {
+                myArrForNumbers.add(Double.parseDouble(arr));
+            } catch (NumberFormatException e) {
+            }
+
+        }
+        System.out.println(mainString.length());
+        double result = (double) myArrForNumbers.get(0);
+        for (int i = 1; i < mainString.length(); i++) {
+            String first = par[i];
+            double val = (double) myArrForNumbers.get(i + 1);
+            switch (first) {
+                case "+":
+                    result += val;
+                    break;
+                case "-":
+                    result -= val;
+                    break;
+                case "*":
+                    result *= val;
+                    break;
+                case "/":
+                    result /= val;
+                    break;
+                default:
+                    System.out.println("arascori info");
+            }
+
+            //System.out.println(mySecondArr);
+
+        }
+        //return result;
+//    public void add(String s){
+//
+//
+//    }
     }
 
-}
+    public static void main(String[] args) {
+            HomeWork_5 obj = new HomeWork_5();
+            obj.getInput();
+            obj.parseInput();
 
+
+    }
+}
